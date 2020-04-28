fd64c0c247aca10542c288858c277e96b50e1e70
bimadmrt <56182626+bimadmrt@users.noreply.github.com>
2020-04-28 23:25:08 +0700

    Update 1

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-  <b-navbar toggleable="lg" type="dark" variant="info fixed-top">
+  <b-navbar toggleable="lg">
     <b-navbar-brand href="#">BimasaktiCooking</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
@@ -17,7 +17,7 @@
                   <b-form-input 
                     size="sm" 
                     class="mr-sm-2" 
-                    placeholder="Search for a meal"
+                    placeholder="Mau makan apa?"
                     v-model="meal"
                     ></b-form-input>
                   <b-button 
@@ -25,10 +25,10 @@
                     class="my-2 my-sm-0" 
                     type="submit" 
                     @click.prevent="getMeal"
-                    >Search</b-button>
+                    >Cari</b-button>
                 </b-nav-form>
 
-        <b-nav-item-dropdown text="Lang" right>
+        <b-nav-item-dropdown text="Bahasa" right>
           <b-dropdown-item href="#">EN</b-dropdown-item>
           <b-dropdown-item href="#">ES</b-dropdown-item>
           <b-dropdown-item href="#">RU</b-dropdown-item>
@@ -64,5 +64,9 @@ export default {
 }    
 </script>
 <style  scoped>
+.navbar {
+  background-color:cadetblue;
+  color: black;
+}
   
 </style>
